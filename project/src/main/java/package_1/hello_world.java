c0a25dbe8c0ae7f41e81563e70a8ccf0fc67068c
Udaya2506 <sonylaxmi2506@gmail.com>
2024-04-30 00:07:13 -0700

    first commit

---
+++ project/src/main/java/package_1/hello_world.java
@@ -0,0 +1,11 @@
+package package_1;
+
+public class hello_world {
+
+	public static void main(String[] args) {
+		System.out.println("hello world ");
+		// TODO Auto-generated method stub
+
+	}
+
+}
